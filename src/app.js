cbef9f228b975b232cdb687b5dc1b72d80881630
Lucas <lucasdevba@gmail.com>
2023-06-28 19:51:59 -0300

    Commented lines of cors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ dotenv.config();
 import './database';
 
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 import helmet from 'helmet';
 
 import homeRoutes from './routes/homeRoutes';
@@ -15,21 +15,21 @@ import tokenRoutes from './routes/tokenRoutes';
 import studentRoutes from './routes/studentRoutes';
 import fileRoutes from './routes/fileRoutes';
 
-const whiteList = [
-  'https://incomparable-griffin-55a7f9.netlify.app/',
-  'http://35.247.250.173/',
-  'http://localhost:3000',
-];
+// const whiteList = [
+//   'https://incomparable-griffin-55a7f9.netlify.app/',
+//   'http://35.247.250.173/',
+//   'http://localhost:3000',
+// ];
 
-const corsOptions = {
-  origin(origin, callback) {
-    if (whiteList.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
+// const corsOptions = {
+//   origin(origin, callback) {
+//     if (whiteList.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// };
 class App {
   constructor() {
     this.app = express();
@@ -38,7 +38,7 @@ class App {
   }
 
   middlewares() {
-    this.app.use(cors(corsOptions));
+    // this.app.use(cors(corsOptions));
     this.app.use(helmet());
     this.app.use(express.urlencoded({ extend: true }));
     this.app.use(express.json());
